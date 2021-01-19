c6da92d44ff59e69bcf7cd88bb76ae6beb565963
Sabine Holler <sabidasa@gmail.com>
2021-01-19 17:24:48 +0100

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// Opening page timeout
+
+setTimeout(function () {
+  document.getElementById('open').style.display='none';
+}, 3000);
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "70px";
+  }
+
+
+
+
+
+//   $.noConflict();
+// jQuery(document).ready(function($) {
+//   $('.menu-toggle').on('click', function() {
+//     $('ul').slideToggle();
+//     $(this).toggleClass('open');
+//   });
+// });
